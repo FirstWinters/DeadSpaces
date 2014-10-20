9bedffb208b456834957bd39c87e16a26d1c7437
Tyler Dantuma <TylerDantuma@gmail.com>
2014-10-20 14:41:47 -0500

    Added MouseDetect and Moving Pieces

---
+++ DeadSpace/Assets/DetectObjects.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DetectObjects : MonoBehaviour {
+
+	Ray myRay;
+
+	Vector3 direction;
+	RaycastHit hit;
+
+	Vector3 pos;
+
+	bool held = true;
+
+	// Update is called once per frame
+	void Update () 
+	{
+		pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+		direction = transform.forward;
+		myRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+		if(Input.GetKeyDown(KeyCode.Mouse0) && !held)
+		{
+			held = true;
+		}
+		else if(Input.GetKeyDown(KeyCode.Mouse0) && held)
+		{
+			held = false;
+		}
+
+		if (Physics.Raycast (myRay, out hit, 200.0f))
+		{
+			/*
+			if(Input.GetKeyDown(KeyCode.Mouse0) && hit.transform.tag == "Player" && !held)
+			{
+				hit.transform.position = myRay.origin + myRay.direction * 10;
+
+			}
+			else if(Input.GetKeyDown(KeyCode.Mouse0)  && hit.transform.tag == "Player" && held)
+			{
+
+			}
+			*/
+
+			if(hit.transform.tag == "Player" && !held)
+			{
+				hit.transform.position = myRay.origin + myRay.direction * 10;
+				
+			}
+		}	
+	}
+	
+}
+
+
---
+++ DeadSpace/Assets/playerScript.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playerScript : MonoBehaviour {
+
+
+
+	bool placed = false;
+	bool want = false;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKeyDown(KeyCode.Mouse0)){
+			want = true;
+			this.transform.position = new Vector3 (transform.position.x, transform.position.y, -5);
+		}
+		if (Input.GetKeyUp(KeyCode.Mouse0)){
+			want = false;
+			this.transform.position = new Vector3 (transform.position.x, transform.position.y, 0);
+		}
+
+	}
+
+	void OnTriggerEnter(Collider c)
+	{
+		if (c.tag == "Square"){
+
+			if(placed == false)
+			{
+				this.transform.position = c.transform.position;
+				placed = true;
+			}
+
+			//do other square stuff
+
+
+
+		}
+	}
+	void OnTriggerExit(Collider c)
+	{
+		placed = false;
+	}
+}
